dc77426a2ad510bdbcda0fade77271a278de5ea4
Rinto <r_yamaguchi@axseed.co.jp>
2023-04-24 11:47:32 +0900

    5th commit

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,5 @@
 import Layout from '../../components/layout';
+import Image from 'next/image';
 import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
